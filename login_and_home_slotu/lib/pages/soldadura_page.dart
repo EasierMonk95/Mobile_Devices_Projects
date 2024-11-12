3703e697346c32c6596cee7c77d15947f7209559
EasierMonk95 <popesinho@gmail.com>
2024-11-12 18:28:52 -0500

    Few changes
    Some changes in order to add the interactive calendar in the pages for reservations.

--- login_and_home_slotu/lib/pages/soldadura_page.dart
+++ login_and_home_slotu/lib/pages/soldadura_page.dart
@@ -1,15 +1,49 @@
 import 'package:flutter/material.dart';
 
-class SoldaduraPage extends StatefulWidget {
-  const SoldaduraPage({super.key});
+class SoldaduraPage extends StatelessWidget {
+  final List<String> days = ["Lun", "Mar", "Mié", "Jue", "Vie", "Sáb", "Dom"];
+  final List<String> hours = [
+    "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00"
+  ];
 
-  @override
-  State<SoldaduraPage> createState() => _SoldaduraPageState();
-}
-
-class _SoldaduraPageState extends State<SoldaduraPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text("Calendario de Soldadura")),
+      body: Column(
+        children: [
+          Row(
+            children: days.map((day) => Expanded(child: Text(day, textAlign: TextAlign.center))).toList(),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: hours.length,
+              itemBuilder: (context, index) {
+                return Row(
+                  children: [
+                    Expanded(child: Text(hours[index], textAlign: TextAlign.center)),
+                    for (int i = 0; i < 7; i++)
+                      Expanded(
+                        child: InkWell(
+                          onTap: () {
+                            // Abre un diálogo para añadir eventos
+                          },
+                          child: Container(
+                            height: 60,
+                            margin: EdgeInsets.all(2),
+                            color: Colors.grey[300],
+                            child: Center(child: Text(" ")),
+                          ),
+                        ),
+                      ),
+                  ],
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
+
--- login_and_home_slotu/lib/repository/calendarific_api.dart
+++ login_and_home_slotu/lib/repository/calendarific_api.dart
@@ -8,14 +8,15 @@ class CalendarificApi2024 {
 
   Future<ApiCalendarific2024Response> getHolidays2024() async {
     final response2024 =
-        await http.get(Uri.parse('https://calendarific.com/api/v2/holidays?&api_key=9gMbnsL1QGCg1MCz79JIsqxhN2RorEKT&country=CO&year=2024'));
+      await http.get(Uri.parse('https://calendarific.com/api/v2/holidays?&api_key=9gMbnsL1QGCg1MCz79JIsqxhN2RorEKT&country=CO&year=2024'));
+      print('Respuesta: ${response2024.body}');
 
-        if (response2024.statusCode == 200){
-          return ApiCalendarific2024Response.fromJson(jsonDecode(response2024.body));
-        }
-        else {
-          throw Exception('Falla al cargar días festivos');
-        }
+      if (response2024.statusCode == 200){
+        return ApiCalendarific2024Response.fromJson(jsonDecode(response2024.body));
+      }
+      else {
+        throw Exception('Falla al cargar días festivos');
+      }
   }
 }
 
@@ -23,13 +24,14 @@ class CalendarificApi2025 {
 
   Future<ApiCalendarific2025Response> getHolidays2025() async {
     final response2025 =
-    await http.get(Uri.parse('https://calendarific.com/api/v2/holidays?&api_key=9gMbnsL1QGCg1MCz79JIsqxhN2RorEKT&country=CO&year=2025'));
+      await http.get(Uri.parse('https://calendarific.com/api/v2/holidays?&api_key=9gMbnsL1QGCg1MCz79JIsqxhN2RorEKT&country=CO&year=2025'));
+      print('Respuesta: ${response2025.body}');
 
-    if (response2025.statusCode == 200){
-      return ApiCalendarific2025Response.fromJson(jsonDecode(response2025.body));
-    }
-    else {
-      throw Exception('Falla al cargar días festivos');
-    }
+      if (response2025.statusCode == 200){
+        return ApiCalendarific2025Response.fromJson(jsonDecode(response2025.body));
+      }
+      else {
+        throw Exception('Falla al cargar días festivos');
+      }
   }
 }
