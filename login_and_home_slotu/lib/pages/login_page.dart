3fa67811f1ee9e684b8a0988e4c20cf4ddcb1103
Dani Marin <dmarinmeneses@gmail.com>
2024-11-19 22:08:01 -0500

    Actualización de login.

--- login_and_home_slotu/lib/pages/login_page.dart
+++ login_and_home_slotu/lib/pages/login_page.dart
@@ -145,7 +145,7 @@ class _LoginPageState extends State<LoginPage> {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => const RegisterPage(),
+                        builder: (context) => const RegisterUserPage(),
                       ),
                     );
                   },
